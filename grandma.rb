834a48dfcaa0179194fdfd34d12b4a905e93fcb3
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-04 04:16:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(str)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-elsif str.scan(/[a-z]/) == nil
+elsif str.scan(/[a-z]/) == []
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
 return "NO, NOT SINCE 1938!"
