395b75de3acf5c55d6b510c5017d7980fa941341
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-04 04:12:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,10 +6,10 @@ def speak_to_grandma(str)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+elsif str.scan(/[a-z]/) == nil
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
-
+return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
